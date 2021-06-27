f533b29cf775093f69111c022bff0b61a4f90526
Erick D <davalos.erick@yahoo.es>
2021-06-27 12:01:28 -0500

    estructura usuario completa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Usuario from './componentes/Usuario';
+import UsuarioFormulario from './componentes/UsuarioFormulario';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <UsuarioFormulario/>
+      <Usuario/>
     </div>
   );
 }
---
+++ src/componentes/UsuarioFormulario.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const UsuarioFormulario = () => {
+    return ( <h1>UsuarioFormulario</h1> );
+}
+ 
+export default UsuarioFormulario;
