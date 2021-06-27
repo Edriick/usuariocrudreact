1c81f25128ee9daa11b1cc05c3308e51d28bd040
Erick D <davalos.erick@yahoo.es>
2021-06-27 13:17:18 -0500

    datos obtenidos desde bd

--- src/componentes/Usuario.js
+++ src/componentes/Usuario.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import UsuarioFormulario from './UsuarioFormulario';
 import {db} from '../firebase'
 
 const Usuario = () => {
 
+    const[usuarios, setUsuarios]= useState([])
     //Añadir Usuario
     const addUsuario = async (usuario) =>{
         await db.collection('TBL_USUARIO').doc().set(usuario)
         console.log('añadido');
     }
 
-    
+    //cargar datos tabla
+    const getUsuarios = async ()=>{
+        db.collection('TBL_USUARIO').onSnapshot((respuesta)=>{
+            const usuarios = [];
+            respuesta.forEach((r) =>{
+                usuarios.push({...r.data(), id_usuario:r.id});
+            });
+            setUsuarios(usuarios);
+        });
+    }
+    useEffect(()=>{
+        getUsuarios()
+    },[])
+
+
     return ( 
     
         <UsuarioFormulario addUsuario={addUsuario}
