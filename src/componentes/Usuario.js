ce8ec078f72ad5ecdb796eb0b9d789ea8204696f
Erick D <davalos.erick@yahoo.es>
2021-06-27 13:32:11 -0500

    reporte de clientes completo

--- src/componentes/Usuario.js
+++ src/componentes/Usuario.js
@@ -27,10 +27,40 @@ const Usuario = () => {
 
 
     return ( 
-    
+    <>
         <UsuarioFormulario addUsuario={addUsuario}
         />
-        
+        <div>
+        <table className="table">
+            <thead className="thead-dark">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Cédula</th>
+                <th scope="col">Teléfono</th>
+                <th scope="col">Mail</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+            </tr>
+            </thead>
+            <tbody>
+                
+                 {usuarios.map(user =>{
+                    return  <tr key={user.id_usuario}>
+                                <th scope="row">{user.id_usuario}</th>
+                                <td>{user.nombre_usuario}</td>
+                                <td>{user.cedula_usuario}</td>
+                                <td>{user.telefono_usuario}</td>
+                                <td>{user.mail_usuario}</td>
+                                <td></td>
+                                <td><i className="material-icons text-danger">close</i></td>
+                            </tr>
+                })}      
+            
+            </tbody>
+        </table>
+    </div>
+    </>
     );
 }
  
